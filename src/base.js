25e0a3f2d1ebdf75b1eac64444702193ce59b0d2
Steven Hofheins <stevenisinsane@gmail.com>
2019-12-20 22:05:45 -0700

    working on react project

--- src/base.js
+++ src/base.js
@@ -6,9 +6,11 @@ const firebaseApp = firebase.initializeApp({
   authDomain: "reactcatchoday.firebaseapp.com",
   databaseURL: "https://reactcatchoday.firebaseio.com",
 });
-
 const base = Rebase.createClass(firebaseApp.database());
 
+// This is a named export
 export { firebaseApp };
 
-export default { base };
+// this is a default export
+export default base;
+
