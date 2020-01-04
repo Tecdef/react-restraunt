29b226244f108fac8028b78669d083190254e10a
Steven Hofheins <stevenisinsane@gmail.com>
2020-01-04 10:22:44 -0700

    make it better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import Router from "./components/Router";
 import "./css/style.css";
-import Router from './components/Router'
 
 render(<Router />, document.querySelector("#main"));
